915a3e88f5ef1bc96e390f51f78b9bb5398202f7
Wellington <juniozguedes@gmail.com>
2023-08-06 02:24:56 -0300

    Moved all lambdas to main.py, changed architecture and deploy

--- src/users/routes.py
+++ src/users/routes.py
@@ -1,11 +1,8 @@
-import json
-import hashlib
 from datetime import timedelta, datetime
 from jose import jwt, JWTError
 from fastapi import APIRouter, Depends, HTTPException, Header, status
 from passlib.context import CryptContext
 from src.users.schemas import CreateUser
-from src.users.aws_lambdas.register_user import lambda_handler
 from src.db.dynamodb.repositories.users_repository import get_user, register
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -37,36 +34,6 @@ def verify_token(authorization: str = Header(...)):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid token",
         ) from exc
-    except Exception as generic_exception:
-        # Handle specific errors if needed
-        error_message = str(generic_exception)
-        raise HTTPException(
-            status_code=500, detail=f"Internal server error: {error_message}"
-        ) from generic_exception
-
-
-@route.post("/lambda/register")
-def lambda_register_user(request: CreateUser):
-    try:
-        # Hash the provided password with the same salt
-        salt = "s135"
-        db_password = request.password + salt
-        hashed_provided = hashlib.md5(db_password.encode()).hexdigest()
-
-        # Assuming `request.email` and `hashed_provided` are defined somewhere
-        user = {
-            "email": request.email,
-            "password": hashed_provided,
-        }
-
-        event = {
-            "body": json.dumps(user)  # Convert the user dictionary to a JSON string
-        }
-
-        # Call Lambda handler function with the constructed event
-        result = lambda_handler(event, None)
-
-        return result
     except Exception as generic_exception:
         # Handle specific errors if needed
         error_message = str(generic_exception)
@@ -108,6 +75,9 @@ def register_user(request: CreateUser):
 def login(request: CreateUser):
     user = get_user(request.email)
     # Verify password
+
+    if not user:
+        return {"message": "User doesn't exist"}
     if not pwd_context.verify(request.password, user["password"]):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
