5866d1221066dc9deeac3bedc86ca860af277055
Filipe Marron <52511902+filipezz@users.noreply.github.com>
2023-08-09 10:41:42 -0300

    fix paths

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.logger import logger as api_logger
 from mangum import Mangum
-from src.users.routes import router as user_router
+from  users.routes import router as user_router
 
 app = FastAPI(title="cataprato_auth", version="v1")
 
