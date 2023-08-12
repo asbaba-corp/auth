c805998df7e741ba00835b9bcfab2f6e5f0ffad1
Filipe Marron <52511902+filipezz@users.noreply.github.com>
2023-08-12 01:44:28 -0300

    fix

--- terraform.tf
+++ terraform.tf
@@ -62,13 +62,13 @@ module "lambda_function" {
 
 
 
-  /* allowed_triggers = {
+ /*    allowed_triggers = {
     APIGatewayAny = {
       service    = "apigateway"
-      source_arn = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${one(data.aws_apigatewayv2_apis.cataprato)}/*/*"
+      source_arn = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${one(data.aws_apigatewayv2_apis.cataprato)}
     }
-  } */
-
+  }  */
+ 
 
   create_lambda_function_url = true
   authorization_type         = "AWS_IAM"
