57ca54598f215f19b0cdd4085409e7bf93164994
Wellington <juniozguedes@gmail.com>
2023-08-15 20:04:41 -0300

    Added new query with partiql

--- src/db/dynamodb/repositories/users_repository.py
+++ src/db/dynamodb/repositories/users_repository.py
@@ -1,24 +1,20 @@
 import uuid
 from datetime import datetime
 from botocore.exceptions import ClientError
-from  users.exceptions import UserNotFoundException
-from  exceptions import DatabaseError
-from  db.dynamodb.connection import dynamo as dynamodb
+from users.exceptions import UserNotFoundException
+from exceptions import DatabaseError
+from db.dynamodb.connection import dynamo as dynamodb
 
 
 def register(user):
     try:
-        response = dynamodb.put_item(
-            TableName="Users",
-            Item={
-                "id": {"S": str(uuid.uuid4())},
-                "email": {"S": user["email"]},
-                "password": {"S": user["password"]},
-                "created_at": {
-                    "S": datetime.utcnow().isoformat(),
-                },
-            },
+        user_id = str(uuid.uuid4())
+        created_at = datetime.utcnow().isoformat()
+        query = (
+            f"INSERT INTO Users (id, email, password, created_at) "
+            f"VALUES ('{user_id}', '{user['email']}', '{user['password']}', '{created_at}')"
         )
+        response = dynamodb.execute_statement(Statement=query)
         return response
     except ClientError as exception:
         raise DatabaseError(
@@ -28,8 +24,11 @@ def register(user):
 
 def get_user(email):
     try:
-        response = dynamodb.get_item(TableName="Users", Key={"email": {"S": email}})
-        user = response.get("Item")
+        query = f"SELECT * FROM Users WHERE email = '{email}'"
+        response = dynamodb.execute_statement(
+            Statement=query,
+        )
+        user = response["Items"][0]
         if not user:
             raise UserNotFoundException(f"User with email '{email}' not found")
         user_data = {
