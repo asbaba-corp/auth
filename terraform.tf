42cfa902c393d3ae24024387f3640aa83fe7aae2
Filipe Marron <52511902+filipezz@users.noreply.github.com>
2023-08-17 16:00:42 -0300

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -42,7 +42,6 @@ module "lambda_function" {
   source_path            = [
                                 {
                                     path          = "${path.module}/src"
-                                    prefix_in_zip = "app"
                                   }
 ]
   store_on_s3 = true
