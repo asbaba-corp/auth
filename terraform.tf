9802427f801fada469e2fd3e821fd2ea664148fc
Filipe Marron <52511902+filipezz@users.noreply.github.com>
2023-08-19 00:36:36 -0300

    Change lambda name (#59)
    * Update terraform.tf
    
    * Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -31,16 +31,14 @@ module "lambda_function" {
   source  = "terraform-aws-modules/lambda/aws"
   version = "5.3.0"
 
-  function_name          = "cataprato-auth-lambda"
-  description            = "Cataprato auth"
+  function_name          = "authentication"
+  description            = "Cataprato authentication"
   handler                = "app.main.handler"
   runtime                = "python3.10"
   ephemeral_storage_size = 512
   architectures          = ["x86_64"]
   publish                = true
   timeout                = 5
-
-
 
    source_path            = [
                            {
